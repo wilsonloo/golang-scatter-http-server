0f11966e2f357427302bc93089dea655af0ad85a
wilsonloo <wilsonloo21@163.com>
2016-05-24 19:19:30 +0800

    启动时可指定 echo 到客户端的消息

--- src/main.go
+++ src/main.go
@@ -1,34 +1,51 @@
-/*
-	说明，参数1表示端口号，如果没有则默认8888
-*/
-
 package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
+var g_echo string
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+
+	// 解析参数, 默认是不会解析的
+	r.ParseForm()
+
+	// 这个写入到w的信息是输出到客户端的
+	fmt.Fprintf(w, g_echo)
+}
+
+func usage() {
+	fmt.Println("========================")
+	fmt.Println("src port [args]")
+}
+
 func main() {
 
-	http.Handle("/test/", http.FileServer(http.Dir("template")))
-	http.Handle("/css/", http.FileServer(http.Dir("template")))
-	http.Handle("/js/", http.FileServer(http.Dir("template")))
-
-	http.HandleFunc("/login/", loginHandler)
-	http.HandleFunc("/", NotFoundHandler)
-
 	var port string
-	if len(os.Args) > 1 {
-		port = os.Args[1]
-	} else {
-		port = "8888"
+
+	g_echo = "server feedback:"
+
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+	port = os.Args[1]
+
+	for i := 2; i < len(os.Args); i++ {
+		g_echo = g_echo + " " + os.Args[i]
 	}
 
-	fmt.Println("http opened at", port)
+	// 设置访问的路由
+	http.HandleFunc("/", sayhelloName)
 
-	addr := ":" + port
-	http.ListenAndServe(addr, nil)
-
+	// 设置监听的端口
+	fmt.Println("listening at port ", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
